dbeea164e9d30348e1029fa4e99a01814790ca0f
Graziela Diehl <diehlgraziela@gmail.com>
2025-05-16 22:32:59 -0300

    api secret config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
   css: ["~/assets/index.css"],
+  runtimeConfig: {
+    apiSecret: process.env.API_SECRET,
+  },
 });
