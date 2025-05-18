fcdf7b9d134aad16298b191d04a6be6bc40eaa0a
Graziela Diehl <diehlgraziela@gmail.com>
2025-05-18 20:05:40 -0300

    fix on messages endpoint

--- server/api/messages/[id].ts
+++ server/api/messages/[id].ts
@@ -1,18 +1,26 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event);
+  const method = event.method;
   const accessToken = config.devMode
     ? config.apiSecret
     : getCookie(event, "access_token");
 
   const { id } = event.context.params!;
 
+  let body;
+
+  if (method !== "GET") {
+    body = await readBody(event);
+  }
+
   const response = await $fetch(
     `https://api.huggy.app/v3/chats/${id}/messages`,
     {
-      method: "GET",
+      method,
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
+      ...(body && { body }),
     }
   );
 
