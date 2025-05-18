bbee5d0e04b0d6120441a4f74d3c67b78bdcb3d4
Graziela Diehl <diehlgraziela@gmail.com>
2025-05-18 14:20:55 -0300

    remove get access token from auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,20 +1,8 @@
-export default defineNuxtRouteMiddleware(async (to, _) => {
+export default defineNuxtRouteMiddleware((to, _) => {
   const accessToken = useCookie("access_token");
 
   if (to.query.code) {
-    try {
-      await $fetch("/api/auth/accessToken", {
-        method: "POST",
-        body: {
-          code: to.query.code,
-        },
-      });
-
-      return navigateTo("/chats");
-    } catch (error) {
-      console.error(error);
-      return navigateTo("/");
-    }
+    return navigateTo("/chats", { replace: true });
   }
 
   if (!accessToken.value && to.path !== "/") {
