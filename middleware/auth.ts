87fe8249db4c2e790567513211d3b38f4fc896b4
Graziela Diehl <diehlgraziela@gmail.com>
2025-05-18 14:30:44 -0300

    test middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware((to, _) => {
   const accessToken = useCookie("access_token");
 
   if (to.query.code) {
-    return navigateTo("/chats", { replace: true });
+    return;
   }
 
   if (!accessToken.value && to.path !== "/") {
