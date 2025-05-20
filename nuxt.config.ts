ab08e853cf3b75d8ea7df653a7e44b4ed6c66952
Graziela Diehl <diehlgraziela@gmail.com>
2025-05-20 09:09:25 -0300

    remove variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,6 @@ export default defineNuxtConfig({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackUrl: process.env.CALLBACK_URL,
-    devMode: process.env.NODE_ENV === "development",
     public: {
       env: process.env.NODE_ENV,
     },
